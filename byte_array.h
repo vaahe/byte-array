69d34f0b47b1a98a21d52ce9b2fb2b69747261f7
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2024-11-02 02:22:40 +0400

    add new members and methods

--- byte_array.h
+++ byte_array.h
@@ -1,10 +1,16 @@
 #ifndef BYTE_ARRAY_H
 #define BYTE_ARRAY_H
 
+#include <vector>
+#include <cstdint>
+#include <ostream>
 class ByteArray
 {
 public:
     ByteArray();
+    ByteArray(size_t size, char ch);
+    ByteArray(const ByteArray &other);
+    ByteArray(ByteArray &&other);
     ~ByteArray();
 
 public:
@@ -12,8 +18,15 @@ public:
     void clear();
     void chop(size_t n);
     void resize(size_t size);
+
+    ByteArray &append(char ch);
+    ByteArray &append(const char *str);
     ByteArray &append(const ByteArray &);
+
+    ByteArray &prepend(char ch);
+    ByteArray &prepend(const char *str);
     ByteArray &prepend(const ByteArray &);
+
     ByteArray &fill(char ch, size_t size = 0);
     ByteArray &remove(size_t pos, size_t len);
 
@@ -36,6 +49,14 @@ public:
 
     // string manipulators
     ByteArray trimmed() const;
+
+    // operators
+    ByteArray &operator=(const ByteArray &other);
+    ByteArray &operator=(ByteArray &&other) noexcept;
+    friend std::ostream &operator<<(std::ostream &out, const ByteArray &byteArray);
+
+private:
+    std::vector<uint8_t> *m_byteArray;
 };
 
 #endif // !BYTE_ARRAY_H
